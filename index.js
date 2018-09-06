5629698b7b2ccb2164aab66c1a81b5cf7f9f2b30
lzricardo <lzricardo.ecomp@gmail.com>
2018-09-05 23:40:49 -0300

    Update .gitignore, package.json, add package-lock.json, mongoose integrate.

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const human = require('./human');
 
 hello('Olá galera do Workshop de Node.js');
 human.hello('Olá galera do Workshop de Node.js');
+
+const mongoose = require('mongoose');
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log('MongoDB connected');
+});
+
+mongoose.connect('mongodb://127.0.0.1/test');
